099b26b890d7d8e48b9e609031a3f45333c9237f
ramenumai <taiki1151001@icloud.com>
2023-01-13 22:04:48 +0900

    名前の変更

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,7 +4,7 @@
             <div class="md:h-96 flex flex-col sm:flex-row rounded-lg overflow-hidden">
             <!-- content - start -->
             <div class="w-full sm:w-1/2 lg:w-2/5 flex flex-col p-4 sm:p-8">
-                <h1 class="text-black text-xl md:text-2xl lg:text-4xl font-bold mb-4">フロナビ！</h1>
+                <h1 class="text-black text-xl md:text-2xl lg:text-4xl font-bold mb-4">フロメグ！</h1>
                 <h2 class="text-black text-lg md:text-xl lg:text-3xl font-bold mb-4">お気に入りの温泉をシェアしませんか？</h2>
 
                 <p class="max-w-md text-gray-400 mb-8">温泉好きが集まり、お気に入りの温泉を共有するサイトです。インスタグラムと同じような機能をそなえていて投稿、いいね、タグ検索、などができます！</p>
